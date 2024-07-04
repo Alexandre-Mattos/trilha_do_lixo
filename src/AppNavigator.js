38eb0ee5d2be6f10f6903095f6bbd5af54aff8ac
Alexandre-Mattos <alexandremattosbc@gmail.com>
2024-07-04 19:51:54 -0300

    [FEATURE] show map

--- src/AppNavigator.js
+++ src/AppNavigator.js
@@ -1,11 +1,11 @@
-import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-import Home from './pages/Home';
-import Configuracao from './pages/Configuracao';
-import Mapa from './pages/Mapa';
+import React from 'react'
+import { NavigationContainer } from '@react-navigation/native'
+import { createStackNavigator } from '@react-navigation/stack'
+import Home from './pages/Home'
+import Configuracao from './pages/Configuracao'
+import Mapa from './pages/Mapa'
 
-const Stack = createStackNavigator();
+const Stack = createStackNavigator()
 
 export default function AppNavigator() {
   return (
@@ -28,5 +28,5 @@ export default function AppNavigator() {
         />
       </Stack.Navigator>
     </NavigationContainer>
-  );
+  )
 }
